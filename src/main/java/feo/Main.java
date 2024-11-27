0962fd840c46f013239090fc8957a0a76e9e7fd0
Bogdan Nikitin <nikitin.bogdan333@yandex.ru>
2024-11-27 22:20:26 +0300

    Use input from stdin, refactor

--- src/main/java/feo/Main.java
+++ src/main/java/feo/Main.java
@@ -1,31 +1,17 @@
 package feo;
 
-import guru.nidi.graphviz.engine.Format;
 import guru.nidi.graphviz.engine.Graphviz;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.nio.charset.StandardCharsets;
 import java.text.ParseException;
 
 public class Main {
-    private static final int WIDTH = 500;
-
     public static void main(String[] args) {
         Graphviz.noHeadless();
-        final Image image;
         try {
-            var in = new ByteArrayInputStream("var a: Array<Array<Array<b>>>;".getBytes(StandardCharsets.UTF_8));
-//            final ArrayDeclaration arrayDeclaration = new Parser().parse(System.in);
-            final ArrayDeclaration arrayDeclaration = new Parser().parse(in);
+            final ArrayDeclaration arrayDeclaration = new Parser().parse(System.in);
             DisplayArrayDeclaration.display(arrayDeclaration);
         } catch (final ParseException e) {
             System.err.println("During parsing an error occurred: " + e);
-            return;
         }
-//        SwingUtilities.invokeLater(() -> {
-//        });
     }
 }
